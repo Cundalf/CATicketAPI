9fffaf52c92292a79cf2655aaace2960b96bcc58
Agustin Cundari <acundari95@gmail.com>
2021-12-16 20:53:42 -0300

    Se emprolija server.model

--- src/models/server.model.ts
+++ src/models/server.model.ts
@@ -13,29 +13,10 @@ import Auth from '../routes/auth.route';
 
 // Logs
 import logger from '../libs/logger.lib';
-import morgan, { StreamOptions } from "morgan";
+import morgan, { StreamOptions } from 'morgan';
 
 // Database Connection
-// import db from '../database/connection.db';
-import { createConnection } from "typeorm";
-
-// Database Models
-// import userModel from './entities/user.model';
-// import ticketUpdatesModel from './entities/ticketUpdates.model';
-// import documentationModel from './entities/documentation.entity';
-// import employeeModel from './entities/employee.model';
-// import employeeDeviceModel from './entities/employeeDevice.model';
-// import employeeDeviceTypeModel from './entities/employeeDeviceType.model';
-// import headquarterModel from './entities/headquarter.model';
-// import orderModel from './entities/order.model';
-// import priorityModel from './entities/priority.model';
-// import responsibleModel from './entities/responsible.model';
-// import sectorModel from './entities/sector.model';
-// import ticketModel from './entities/ticket.model';
-// import ticketCategoryModel from './entities/ticketCategory.model';
-// import ticketStateModel from './entities/ticketState.model';
-// import ticketSubCategoryModel from './entities/ticketSubCategory.entity';
-// import ticketTaskModel from './entities/ticketTask.model';
+import { createConnection } from 'typeorm';
 
 // Type para gestionar las rutas.
 type PathsAPI = {
@@ -73,7 +54,7 @@ class Server {
             employeeDevice: '/employee/device'
         };
 
-        logger.info("Server instanciado");
+        logger.info('Server instanciado');
 
         this.middlewares();
         this.logging();
@@ -82,81 +63,21 @@ class Server {
     }
 
     private database(): void {
-        
+
         createConnection({
-            type: "mysql",
+            type: 'mysql',
             host: process.env.DB_HOST,
             port: parseInt(process.env.DB_PORT!),
             username: process.env.DB_USER,
             password: process.env.DB_PASS,
             database: process.env.DB_NAME,
             entities: [
-                __dirname + "/entities/*.entity.js"
+                __dirname + '/entities/*.entity.js'
             ],
             synchronize: true,
         }).then(connection => {
-            console.log("Conectado");
+            console.log('Conectado');
         }).catch(error => console.log(error));
-        
-        //this.connectDB();
-        //this.syncDB();
-    }
-
-    private async connectDB() {
-        // try {
-        //     await db.authenticate();
-        //     logger.info('Database online');
-        // } catch (err: unknown) {
-        //     if (err instanceof Error)
-        //         throw new Error(err.message);
-        //     else
-        //         throw new Error('Database connection error');
-        // }
-    }
-
-    private async syncDB() {
-        try {
-            // //const config = { force: true };
-            // //const config = { alter: true };
-            // const config = {};
-
-            // // User Models
-            // await userModel.sync(config);
-
-            // // Aux Models
-            // await headquarterModel.sync(config);
-            // await sectorModel.sync(config);
-            // await priorityModel.sync(config);
-
-            // // Employee Models
-            // await employeeDeviceTypeModel.sync(config);
-            // await employeeModel.sync(config);
-            // await employeeDeviceModel.sync(config);
-
-            // // Ticket Aux Models
-            // await ticketCategoryModel.sync(config);
-            // await ticketSubCategoryModel.sync(config);
-            // await ticketStateModel.sync(config);
-            // await ticketTaskModel.sync(config);
-
-            // // Ticket Dependencies
-            // await documentationModel.sync(config);
-            // await orderModel.sync(config);
-
-            // // Main Ticket Model
-            // await ticketModel.sync(config);
-
-            // // Ticket Dependencies
-            // await responsibleModel.sync(config);
-            // await ticketUpdatesModel.sync(config);
-
-            console.log('Database OK');
-        } catch (err: unknown) {
-            if (err instanceof Error)
-                throw new Error(err.message);
-            else
-                throw new Error('Error in database schema');
-        }
     }
 
     // Configura los middlewares que necesitamos en express
@@ -184,7 +105,7 @@ class Server {
             {
                 windowMs: 15 * 60 * 1000, // 15 minutes 
                 max: 200, // limit each IP to 200 requests per windowMs
-                message: "Too many accounts created from this IP, please try again after an hour"
+                message: 'Too many accounts created from this IP, please try again after an hour'
             }
         );
         this.app.use(limiter);
@@ -196,7 +117,7 @@ class Server {
             createParentPath: true
         }));
 
-        logger.info("Middlewares configurados");
+        logger.info('Middlewares configurados');
     }
 
     // Configura el sistema de logging (morgan + winston)
@@ -208,12 +129,12 @@ class Server {
 
         // Solo trackeo en desarrollo las peticiones.
         const skip = () => {
-            const env = process.env.NODE_ENV || "development";
-            return env !== "development";
+            const env = process.env.NODE_ENV || 'development';
+            return env !== 'development';
         };
 
         const morganMiddleware = morgan(
-            ":method :url :status :res[content-length] - :response-time ms",
+            ':method :url :status :res[content-length] - :response-time ms',
             { stream, skip }
         );
 
